0089f7c748792f4df4a3fb67d75cc28d3b646fa8
ahadalkhaled <aalkhaled55@gmail.com>
2018-11-15 09:39:35 -0800

    submission

--- components.py
+++ components.py
@@ -5,18 +5,27 @@ class Store():
         Initializes a new store with a name.
         """
         # your code goes here!
+        self.name = name
+        self.products: []
 
     def add_product(self, product):
         """
         Adds a product to the list of products in this store.
         """
         # your code goes here!
+        self.product.append(product)
 
     def print_products(self):
         """
         Prints all the products of this store in a nice readable format.
         """
         # your code goes here!
+        print()
+        print("%s:" % self.name)
+        for product in self.product:
+            print(product)
+        print()
+
 
 
 class Product():
@@ -25,9 +34,14 @@ class Product():
         Initializes a new product with a name, a description, and a price.
         """
         # your code goes here!
+        self.name = name
+        self.description = description
+        self.price = price
+
 
     def __str__(self):
         # your code goes here!
+        return "\tProduct Name: %s\n\tDescription: %s\n\tPrice: %s KWD" % (self.name, self.description, self.price)
 
 
 class Cart():
@@ -36,27 +50,52 @@ class Cart():
         Initializes a new cart with an empty list of products.
         """
         # your code goes here!
+        self.products: []
 
     def add_to_cart(self, product):
         """
         Adds a product to this cart.
         """
         # your code goes here!
+        self.products.append(product)
 
     def get_total_price(self):
         """
         Returns the total price of all the products in this cart.
         """
         # your code goes here!
+        price = 0
+        for product in self.products:
+            price += product.price
+        return price
 
     def print_receipt(self):
         """
         Prints the receipt in a nice readable format.
         """
         # your code goes here!
+        print("This is your receipt:")
+        for products in self.products:
+            print(product)
+
 
     def checkout(self):
         """
         Does the checkout.
         """
         # your code goes here!
+        print()
+        self.print_receipt()
+        confirm = input("Confirm?(yes/no)")
+        if confirm.lower() == yes:
+            self.products = []
+            print("Your order has been placed.")
+
+        else:
+            print("Your order has been cancelled.")
+
+
+
+
+
+
